d5101b6dc2c3ea6f37f5b4839679644e7fa2f7ea
elonniu <elonniu@amazon.com>
2022-08-01 21:51:07 +0800

    improved style and upgrade version of dependencies

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -3,7 +3,7 @@
 	import Chart from "$lib/components/Chart.svelte"
 
 	let pets = [
-		{name: 'dog', votes: 0}, 
+		{name: 'dog', votes: 0},
 		{name: 'cat', votes: 0},
 		{name: 'bird', votes: 0}
 	]
@@ -14,18 +14,18 @@
   <title>Serverless Polling App</title>
 </svelte:head>
 
-<div class="flex basic-2 my-4 justify-between items-end">
+<div class="flex flex-col md:flex-row basic-2 my-4 justify-between items-end">
   <h1 class="text-4xl font-bold">What is your favorite pet?</h1>
   <p class="px-2">Total votes: {pets.map(p => p.votes).reduce((x, y) => x + y)}</p>
 </div>
 
-<div class="flex flex-row justify-between">
-	{#each pets as pet} 
+<div class="flex flex-col md:flex-row">
+	{#each pets as pet}
 		<Vote vote={pet.name} bind:value={pet.votes} />
 	{/each}
 </div>
 
-<div class="h-96 shadow-xl my-2 bg-base-100 rounded-2xl">
+<div class="h-96 shadow-xl my-2 bg-base-100 rounded-2xl p-5">
 	<Chart data={pets} />
 </div>
 
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,6 +2,16 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+				'2xl': '6rem',
+			},
+		},
 		extend: {}
 	},
 
