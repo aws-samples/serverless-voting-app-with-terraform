332f1b435c39fff67dbc61aac2ae4fa6a7ebf41c
Harold Sun <sunhua@amazon.com>
2022-10-27 06:52:13 +0000

    setup main branch as starter template

--- services/outputs.tf
+++ services/outputs.tf
@@ -1,12 +1,19 @@
-output "apigw_endpoint" {
-  value = module.api_gateway.apigatewayv2_api_api_endpoint
-}
+# --------------------------------------------------------- 
+# Module 2 - Backend APIs
+# ---------------------------------------------------------
 
-output "iotcore_endpoint" {
-  value = data.aws_iot_endpoint.current.endpoint_address
-}
+# output "apigw_endpoint" {
+#   value = module.api_gateway.apigatewayv2_api_api_endpoint
+# }
 
-output "cognito_identity_pool_id" {
-  value = aws_cognito_identity_pool.main.id
-}
+# ---------------------------------------------------------
+# Module 4 - Realtime Updates
+# ---------------------------------------------------------
 
+# output "iotcore_endpoint" {
+#   value = data.aws_iot_endpoint.current.endpoint_address
+# }
+
+# output "cognito_identity_pool_id" {
+#   value = aws_cognito_identity_pool.main.id
+# }
